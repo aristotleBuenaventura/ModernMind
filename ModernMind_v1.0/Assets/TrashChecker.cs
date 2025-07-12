21331efdd51d00c3c1ca74462b931b9e99305c3e
aris <aaristotlee@gmail.com>
2025-07-12 19:54:01 +0800

    trash logic 80%

---
+++ ModernMind_v1.0/Assets/TrashChecker.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TrashChecker : MonoBehaviour
+{
+    public string checkerValue;
+    public GameObject OpenLid, CloseLid, Buttons;
+    public ShowUI Bag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerPrefs.SetString("CheckerValue", checkerValue);
+            PlayerPrefs.Save(); // Optional, ensures data is written immediately
+            Bag.UICanvasShow();
+            CloseLid.SetActive(false);
+            OpenLid.SetActive(true);
+            Buttons.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Bag.UICanvasClose();
+            CloseLid.SetActive(true);
+            OpenLid.SetActive(false);
+            Buttons.SetActive(false);
+            
+        }
+    }
+}
