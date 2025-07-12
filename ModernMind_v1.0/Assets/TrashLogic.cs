67bc0f5b8d5115cb98880dbc6063e96986bcacc1
aris <aaristotlee@gmail.com>
2025-07-12 21:35:43 +0800

    throw trash done

--- ModernMind_v1.0/Assets/TrashLogic.cs
+++ ModernMind_v1.0/Assets/TrashLogic.cs
@@ -4,18 +4,39 @@ public class TrashLogic : MonoBehaviour
 {
     public TrashResultClose result;
     public ShowUI bag;
-    public GameObject BurgerParent, LibroParent, CupParent, BlueButton, GreenButton, BlackButton;
+    public GameObject BurgerParent, LibroParent, CupParent, BlueButton, GreenButton, BlackButton, check, circles;
+    public ShowUI task;
+    private bool blueDone = false;
+    private bool greenDone = false;
+    private bool blackDone = false;
+
+    private void CheckAllDone()
+    {
+        if (blueDone && greenDone && blackDone)
+        {
+            Debug.Log("ALLDONE");
+            result.ResultClose();
+            bag.UICanvasClose();
+            task.UICanvasShow();
+            check.SetActive(true);
+            circles.SetActive(false);
+
+
+        }
+    }
 
     public void blueLogic()
     {
         string savedValue = PlayerPrefs.GetString("CheckerValue");
         bag.UICanvasClose();
-        
+
         if (savedValue == "blue")
         {
             result.TumpakShow();
             BlueButton.SetActive(false);
             LibroParent.SetActive(false);
+            blueDone = true;
+            CheckAllDone();
         }
         else
         {
@@ -27,12 +48,14 @@ public class TrashLogic : MonoBehaviour
     {
         string savedValue = PlayerPrefs.GetString("CheckerValue");
         bag.UICanvasClose();
-        
+
         if (savedValue == "green")
         {
             result.TumpakShow();
             GreenButton.SetActive(false);
             BurgerParent.SetActive(false);
+            greenDone = true;
+            CheckAllDone();
         }
         else
         {
@@ -44,17 +67,18 @@ public class TrashLogic : MonoBehaviour
     {
         string savedValue = PlayerPrefs.GetString("CheckerValue");
         bag.UICanvasClose();
-        
+
         if (savedValue == "black")
         {
             result.TumpakShow();
             BlackButton.SetActive(false);
             CupParent.SetActive(false);
+            blackDone = true;
+            CheckAllDone();
         }
         else
         {
             result.MaliShow();
         }
     }
-
 }
