9af687bcf634e1bc1e2e78f09a15bef0c9c7cedc
aris <aaristotlee@gmail.com>
2025-07-11 23:41:41 -0500

    done canvas to fifth

---
+++ ModernMind_v1.0/Assets/Scene1CanvasManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene1CanvasManager : MonoBehaviour
+{
+    public GameObject FirstCanvas, SecondCanvas, ThirdCanvas, ForthCanvas, FifthCanvas;
+    public GameObject FirstCamera, JoseCamera, Controller;
+
+    void Start()
+    {
+        FirstCanvasShow();
+        Controller.SetActive(false);
+        FirstCamera.SetActive(true);
+        JoseCamera.SetActive(false);
+        SecondCanvas.SetActive(false);
+        ThirdCanvas.SetActive(false);
+        ForthCanvas.SetActive(false);
+        FifthCanvas.SetActive(false);
+    }
+
+    public void FirstCanvasShow()
+    {
+        ShowDialogue(FirstCanvas);
+    }
+
+    public void SecondCanvasShow()
+    {
+        ShowDialogue(SecondCanvas);
+    }
+
+    public void ThirdCanvasShow()
+    {
+        ShowDialogue(ThirdCanvas);
+    }
+
+    public void ForthCanvasShow()
+    {
+        ShowDialogue(ForthCanvas);
+    }
+
+    public void FifthCanvasShow()
+    {
+        ShowDialogue(FifthCanvas);
+        FirstCamera.SetActive(false);
+        JoseCamera.SetActive(true);
+        Controller.SetActive(true);
+    }
+
+    public void FifthCanvasClose()
+    {
+        FifthCanvas.SetActive(false);
+    }
+
+    private void ShowDialogue(GameObject dialogue)
+    {
+        FirstCanvas.SetActive(false);
+        SecondCanvas.SetActive(false);
+        ThirdCanvas.SetActive(false);
+        ForthCanvas.SetActive(false);
+        FifthCanvas.SetActive(false);
+        dialogue.SetActive(true);
+    }
+}
