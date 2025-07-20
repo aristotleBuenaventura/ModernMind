cabd79afeaff26ba11879cedb3d6ecc33e223107
aris <aaristotlee@gmail.com>
2025-07-20 17:17:18 +0800

    retry working

---
+++ ModernMind_v1.0/Assets/Scene1_HopscotchCanvasManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene1_HopscotchCanvasManager : MonoBehaviour
+{
+
+    public GameObject FirstCanvas, SecondCanvas, ThirdCanvas, controls;
+
+    void Start()
+    {
+        FirstCanvasShow();
+        SecondCanvas.SetActive(false);
+        ThirdCanvas.SetActive(false);
+        controls.SetActive(false);
+    }
+
+    public void FirstCanvasShow()
+    {
+        ShowDialogue(FirstCanvas);
+    }
+
+    public void SecondCanvasShow()
+    {
+        ShowDialogue(SecondCanvas);
+    }
+
+    public void ThirdCanvasShow()
+    {
+        ShowDialogue(ThirdCanvas);
+    }
+
+    public void ThirdCanvasClose()
+    {
+        ThirdCanvas.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    private void ShowDialogue(GameObject dialogue)
+    {
+        FirstCanvas.SetActive(false);
+        SecondCanvas.SetActive(false);
+        ThirdCanvas.SetActive(false);
+
+        dialogue.SetActive(true);
+    }
+
+
+
+}
