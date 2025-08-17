4f47890b0145a7a22aac486289beb4648f5d3c35
aris <aaristotlee@gmail.com>
2025-08-17 09:53:19 +0800

    added 10 basura

--- ModernMind_v1.0/Assets/PickUpItems.cs
+++ ModernMind_v1.0/Assets/PickUpItems.cs
@@ -4,41 +4,37 @@ using System.Collections.Generic;
 
 public class PickUpItems : MonoBehaviour
 {
-    [Header("Collected Trash Placeholders")]
-    public GameObject trash1;
-    public GameObject trash2;
-    public GameObject trash3;
+    [Header("Collected Trash Placeholders (3D)")]
+    public List<GameObject> trashPlaceholders;  // for showing collected trash objects in world
 
     [Header("Trash UI Images")]
-    public GameObject trash1Image;
-    public GameObject trash2Image;
-    public GameObject trash3Image;
+    public List<GameObject> trashImages; // for showing corresponding UI images
 
+    [Header("Other UI Objects")]
     public GameObject trashCircle;
-
     public GameObject check;
     public ShowUI taskCanvas;
-    private Dictionary<string, GameObject> trashMap;
-    private Dictionary<string, GameObject> imageMap;
+
+    private Dictionary<string, GameObject> trashMap = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> imageMap = new Dictionary<string, GameObject>();
 
     private HashSet<string> collectedTrash = new HashSet<string>();
 
     private void Start()
     {
         trashCircle.SetActive(false);
-        trashMap = new Dictionary<string, GameObject>
-        {
-            { "trash1", trash1 },
-            { "trash2", trash2 },
-            { "trash3", trash3 }
-        };
 
-        imageMap = new Dictionary<string, GameObject>
+        // Build maps dynamically from list indexes
+        for (int i = 0; i < trashPlaceholders.Count; i++)
         {
-            { "trash1", trash1Image },
-            { "trash2", trash2Image },
-            { "trash3", trash3Image }
-        };
+            string tagName = "trash" + (i + 1); // expects tags like trash1, trash2, trash3...
+
+            if (trashPlaceholders[i] != null)
+                trashMap[tagName] = trashPlaceholders[i];
+
+            if (i < trashImages.Count && trashImages[i] != null)
+                imageMap[tagName] = trashImages[i];
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -49,19 +45,20 @@ public class PickUpItems : MonoBehaviour
             other.gameObject.SetActive(false);
             collectedTrash.Add(other.tag);
 
-            // Show corresponding image
+            // Show corresponding UI image
             if (imageMap.ContainsKey(other.tag))
             {
                 imageMap[other.tag].SetActive(true);
             }
 
-            // Optional: show and hide 3D placeholder object
+            // Show & hide placeholder
             GameObject displayObject = trashMap[other.tag];
             if (displayObject != null)
             {
                 StartCoroutine(ShowAndHide(displayObject, 3f));
             }
 
+            // Check if all trash collected
             if (collectedTrash.Count == trashMap.Count)
             {
                 Debug.Log("ALLDONE");
