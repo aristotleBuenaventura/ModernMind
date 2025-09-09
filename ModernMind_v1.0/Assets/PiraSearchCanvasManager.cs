21c7707f0edfaf99737c98e2e0e4457e08a67c76
aris <aaristotlee@gmail.com>
2025-09-09 14:07:54 +0800

    for testing

--- ModernMind_v1.0/Assets/PiraSearchCanvasManager.cs
+++ ModernMind_v1.0/Assets/PiraSearchCanvasManager.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 public class PiraSearchCanvasManager : MonoBehaviour
 {
     public GameObject Loading, FirstCanvas, SecondCanvas, ThirdCanvas, FourthCanvas, FifthCanvas, SixthCanvas, SeventhCanvas, EighthCanvas;
-    public GameObject puzzleUiPart1, puzzleUiPart2, Puzzle3dPart1, Puzzle3dPart2;
+    public GameObject puzzleUiPart1, puzzleUiPart2, puzzleUiPart3, Puzzle3dPart1, Puzzle3dPart2, Puzzle3dPart3;
     public TimerHopscotch timer;
-    public GameObject set1MissingLetter, set2MissingLetter, Letters1, Letters2, cameraTop, Letter1UI, Letter2UI;
-    public GameObject dummyCamera, cameraReal;
+    public GameObject set1MissingLetter, set2MissingLetter, Letters1, Letters2, cameraTop, Letter1UI, Letter2UI, Letters3, Letter3UI, set3MissingLetter;
+    public GameObject dummyCamera, cameraReal, dummyCamera2;
 
     void Start()
     {
@@ -76,6 +76,29 @@ public class PiraSearchCanvasManager : MonoBehaviour
         Letters2.SetActive(true);
         cameraTop.SetActive(false);
         Letter2UI.SetActive(true);
+    }
+
+    public void PuzzlePart3Show()
+    {
+        set2MissingLetter.SetActive(false);
+        Letters2.SetActive(false);
+        cameraTop.SetActive(true);
+        Puzzle3dPart3.SetActive(true);
+        puzzleUiPart3.SetActive(true);
+        puzzleUiPart2.SetActive(false);
+        Puzzle3dPart2.SetActive(false);
+        cameraReal.transform.position = dummyCamera2.transform.position;
+        cameraReal.transform.rotation = dummyCamera2.transform.rotation;
+
+    }
+
+    public void MissingLetterPart3Show()
+    {
+        Puzzle3dPart3.SetActive(false);
+        set3MissingLetter.SetActive(true);
+        Letters3.SetActive(true);
+        cameraTop.SetActive(false);
+        Letter3UI.SetActive(true);
     }
 
 
