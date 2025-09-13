fb5641b03b2629d50af47b96e07ced32a754c6f8
aris <aaristotlee@gmail.com>
2025-09-13 19:53:57 +0800

    updates

--- ModernMind_v1.0/Assets/BoxOpener.cs
+++ ModernMind_v1.0/Assets/BoxOpener.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class BoxOpener : MonoBehaviour
 {
     private bool isOpen = false;
+    public GameObject Products, box;
 
     public void OpenBox()
     {
         if (isOpen) return; // already opened
         isOpen = true;
-
+        Products.SetActive(true);
+        box.SetActive(false);
         Debug.Log($"{gameObject.name} opened!");
         // TODO: play animation, disable collider, etc.
     }
