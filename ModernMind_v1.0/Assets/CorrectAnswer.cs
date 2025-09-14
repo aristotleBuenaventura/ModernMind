7f26fd43dd22ef6f71bb0b1f9fd61a69f462b996
aris <aaristotlee@gmail.com>
2025-09-14 20:59:51 +0800

    save

--- ModernMind_v1.0/Assets/CorrectAnswer.cs
+++ ModernMind_v1.0/Assets/CorrectAnswer.cs
@@ -7,6 +7,7 @@ public class CorrectAnswer : MonoBehaviour
     public PlatformCounter counter;
     public KeyManager key;
     public CoinsValue coins;
+    public TrackCoins newCoins;
     private bool hasTriggered = false; // Ensures it only runs once
 
     private void OnTriggerEnter(Collider other)
@@ -19,6 +20,7 @@ public class CorrectAnswer : MonoBehaviour
             correctCanvas.SetActive(true);
             key.IncrementKey(1);
             coins.IncrementScore(3);
+            newCoins.IncrementScore(3);
             counter.counterPlus();
             wrong.SetActive(false);
             questionCanvas.SetActive(false);
