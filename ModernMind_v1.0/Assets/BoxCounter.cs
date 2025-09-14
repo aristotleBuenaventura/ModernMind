5b0bea7865f12147a06760010760e186de5ed409
aris <aaristotlee@gmail.com>
2025-09-14 20:34:40 +0800

    save

--- ModernMind_v1.0/Assets/BoxCounter.cs
+++ ModernMind_v1.0/Assets/BoxCounter.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class BoxCounter : MonoBehaviour
 {
     private int counter = 0;
+    public GameObject Results;
 
     public void CounterIncrement()
     {
         counter++;
         if (counter == 5)
         {
+            Results.SetActive(true);
             Debug.Log("BOXES DONE!!!");
         }
     }
--- ModernMind_v1.0/Assets/Scene2CanvasManager.cs
+++ ModernMind_v1.0/Assets/Scene2CanvasManager.cs
@@ -2,15 +2,127 @@ using UnityEngine;
 
 public class Scene2CanvasManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject loading, FirstCanvas, SecondCanvas, ThirdCanvas, Loading2Canvas, FourthCanvas, FifthCanvas, SixthCanvas, SeventhCanvas, EighthCanvas, NinthCanvas, TenthCanvas, EleventhCanvas, TwelfthCanvas, ThirteenthCanvas;
+    public ShowUI taskCanvas;
+    public GameObject unboxShip;
+    public TimerDisplay timer;
+
     void Start()
     {
-        
+        loading.SetActive(true);
+        FirstCanvas.SetActive(false);
+        SecondCanvas.SetActive(false);
+        ThirdCanvas.SetActive(false);
+        FourthCanvas.SetActive(false);
+        FifthCanvas.SetActive(false);
+        SixthCanvas.SetActive(false);
+        SeventhCanvas.SetActive(false);
+        EighthCanvas.SetActive(false);
+        NinthCanvas.SetActive(false);
+        TenthCanvas.SetActive(false);
+        EleventhCanvas.SetActive(false);
+        TwelfthCanvas.SetActive(false);
+        ThirteenthCanvas.SetActive(false);
+        Loading2Canvas.SetActive(false);
+        unboxShip.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FirstCanvasShow()
     {
-        
+        ShowDialogue(FirstCanvas);
+    }
+
+    public void SecondCanvasShow()
+    {
+        ShowDialogue(SecondCanvas);
+    }
+
+    public void ThirdCanvasShow()
+    {
+        ShowDialogue(ThirdCanvas);
+    }
+
+    public void Loading2Show()
+    {
+        ShowDialogue(Loading2Canvas);
+    }
+
+    public void FourthCanvasShow()
+    {
+        ShowDialogue(FourthCanvas);
+    }
+
+    public void FifthCanvasShow()
+    {
+        ShowDialogue(FifthCanvas);
+    }
+
+    public void SixthCanvasShow()
+    {
+        ShowDialogue(SixthCanvas);
+    }
+
+    public void SeventhCanvasShow()
+    {
+        ShowDialogue(SeventhCanvas);
+    }
+
+    public void EighthCanvasShow()
+    {
+        ShowDialogue(EighthCanvas);
+    }
+
+    public void NinthCanvasShow()
+    {
+        ShowDialogue(NinthCanvas);
+    }
+
+    public void TenthCanvasShow()
+    {
+        ShowDialogue(TenthCanvas);
+    }
+
+    public void EleventhCanvasShow()
+    {
+        ShowDialogue(EleventhCanvas);
+    }
+
+    public void TwelfthCanvasShow()
+    {
+        ShowDialogue(TwelfthCanvas);
+    }
+
+    public void ThirteenthCanvasShow()
+    {
+        ShowDialogue(ThirteenthCanvas);
+    }
+
+    public void ThirteenthCanvasClose()
+    {
+        ThirteenthCanvas.SetActive(false);
+        taskCanvas.UICanvasShow();
+        unboxShip.SetActive(true);
+        timer.StartTimer();
+    }
+
+    private void ShowDialogue(GameObject dialogue)
+    {
+        loading.SetActive(false);
+        FirstCanvas.SetActive(false);
+        SecondCanvas.SetActive(false);
+        ThirdCanvas.SetActive(false);
+        Loading2Canvas.SetActive(false);
+        FourthCanvas.SetActive(false);
+        FifthCanvas.SetActive(false);
+        SixthCanvas.SetActive(false);
+        SeventhCanvas.SetActive(false);
+        EighthCanvas.SetActive(false);
+        NinthCanvas.SetActive(false);
+        TenthCanvas.SetActive(false);
+        EleventhCanvas.SetActive(false);
+        TwelfthCanvas.SetActive(false);
+        ThirteenthCanvas.SetActive(false);
+
+        dialogue.SetActive(true);
     }
 }
