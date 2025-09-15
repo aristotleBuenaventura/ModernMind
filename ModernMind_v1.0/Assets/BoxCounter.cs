a6889bb799afd6cd5918d0810d2d06bfc3d5843e
aris <aaristotlee@gmail.com>
2025-09-15 23:09:28 +0800

    save

--- ModernMind_v1.0/Assets/BoxCounter.cs
+++ ModernMind_v1.0/Assets/BoxCounter.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BoxCounter : MonoBehaviour
 {
     private int counter = 0;
-    public GameObject Results;
+    public GameObject Results, boxCheck;
 
     public void CounterIncrement()
     {
@@ -11,6 +11,7 @@ public class BoxCounter : MonoBehaviour
         if (counter == 5)
         {
             Results.SetActive(true);
+            boxCheck.SetActive(true);
             Debug.Log("BOXES DONE!!!");
         }
     }
