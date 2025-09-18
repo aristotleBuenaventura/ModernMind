be57bf64f3b282738abec9a81ecafd601b3a39f4
aris <aaristotlee@gmail.com>
2025-09-18 23:36:42 +0800

    save

---
+++ ModernMind_v1.0/Assets/TrapLogic.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class TrapLogic : MonoBehaviour
+{
+    private bool canCollide = true; // flag to control collision cooldown
+    public float cooldownTime = 2f; // seconds delay
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && canCollide)
+        {
+            Debug.Log("Detected");
+            StartCoroutine(CollisionCooldown());
+        }
+    }
+
+    private IEnumerator CollisionCooldown()
+    {
+        canCollide = false;              // disable collision detection
+        yield return new WaitForSeconds(cooldownTime); // wait for 2s
+        canCollide = true;               // re-enable collision detection
+    }
+}
