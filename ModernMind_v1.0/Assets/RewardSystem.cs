7cd5564f988e38cbc331ea61499671bedc1f05c1
aris <aaristotlee@gmail.com>
2025-09-28 13:27:07 +0800

    save

--- ModernMind_v1.0/Assets/RewardSystem.cs
+++ ModernMind_v1.0/Assets/RewardSystem.cs
@@ -1,29 +1,79 @@
 using UnityEngine;
+using TMPro;
 
 public class RewardSystem : MonoBehaviour
 {
-    private const string HintKey = "Hint";
-    private const string FreezeKey = "Freeze";
-    private const string SkipKey = "Skip";
+    // Keys for PlayerPrefs
+    private const string HintKey = "hint";
+    private const string FreezeKey = "freeze";
+    private const string SkipKey = "skip";
+
+    public TextMeshProUGUI hintText;
+    public TextMeshProUGUI freezeText;
+    public TextMeshProUGUI skipText;
+
+    public GameObject hintImage, freezeImage, skipImage;
+
+    private void Start()
+    {
+        // Initialize UI with saved counts
+        UpdateHintText();
+        UpdateFreezeText();
+        UpdateSkipText();
+    }
 
     public void Hint()
     {
         int currentHint = PlayerPrefs.GetInt(HintKey, 0);
-        PlayerPrefs.SetInt(HintKey, currentHint + 1);
+        currentHint++;
+        PlayerPrefs.SetInt(HintKey, currentHint);
         PlayerPrefs.Save();
+        hintImage.SetActive(false);
+
+        UpdateHintText();
     }
 
     public void Freeze()
     {
         int currentFreeze = PlayerPrefs.GetInt(FreezeKey, 0);
-        PlayerPrefs.SetInt(FreezeKey, currentFreeze + 1);
+        currentFreeze++;
+        PlayerPrefs.SetInt(FreezeKey, currentFreeze);
         PlayerPrefs.Save();
+        freezeImage.SetActive(false);
+        UpdateFreezeText();
     }
 
     public void Skip()
     {
         int currentSkip = PlayerPrefs.GetInt(SkipKey, 0);
-        PlayerPrefs.SetInt(SkipKey, currentSkip + 1);
+        currentSkip++;
+        PlayerPrefs.SetInt(SkipKey, currentSkip);
         PlayerPrefs.Save();
+        skipImage.SetActive(false);
+        UpdateSkipText();
+    }
+
+    private void UpdateHintText()
+    {
+        if (hintText != null)
+        {
+            hintText.text = "x" + PlayerPrefs.GetInt(HintKey, 0);
+        }
+    }
+
+    private void UpdateFreezeText()
+    {
+        if (freezeText != null)
+        {
+            freezeText.text = "x" + PlayerPrefs.GetInt(FreezeKey, 0);
+        }
+    }
+
+    private void UpdateSkipText()
+    {
+        if (skipText != null)
+        {
+            skipText.text = "x" + PlayerPrefs.GetInt(SkipKey, 0);
+        }
     }
 }
