7417c8882cb48151384fd1a0e24539102fc270dd
Aristotle Buenaventura <79913411+aristotleBuenaventura@users.noreply.github.com>
2025-10-24 10:33:41 +0800

    save

--- ModernMind_v1.0/Assets/Scene3Redlight.cs
+++ ModernMind_v1.0/Assets/Scene3Redlight.cs
@@ -5,7 +5,7 @@ public class Scene3Redlight : MonoBehaviour
     public GameObject loading, First, Second, Third, loading2, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Eleventh, Twelveth;
     public GameObject Thirteenth, Fourteenth, Fifteenth;
     public GameObject loading3, Sixteenth, Seventeenth, Eighteenth, Ninteenth;
-
+    public GameObject c20, c21, c22, c23, c24, c25, c26, c27, c28, c29;
 
 
     void Start()
@@ -31,6 +31,16 @@ public class Scene3Redlight : MonoBehaviour
         Seventeenth.SetActive(false);
         Eighteenth.SetActive(false);
         Ninteenth.SetActive(false);
+        c20.SetActive(false);
+        c21.SetActive(false);
+        c22.SetActive(false);
+        c23.SetActive(false);
+        c24.SetActive(false);
+        c25.SetActive(false);
+        c26.SetActive(false);
+        c27.SetActive(false);
+        c28.SetActive(false);
+        c29.SetActive(false);
     }
 
     public void FirstCanvasShow()
@@ -153,6 +163,61 @@ public class Scene3Redlight : MonoBehaviour
         Ninteenth.SetActive(false);
     }
 
+    public void c20CanvasShow()
+    {
+        ShowDialogue(c20);
+    }
+
+    public void c21CanvasShow()
+    {
+        ShowDialogue(c21);
+    }
+
+    public void c22CanvasShow()
+    {
+        ShowDialogue(c22);
+    }
+
+    public void c23CanvasShow()
+    {
+        ShowDialogue(c23);
+    }
+
+    public void c24CanvasShow()
+    {
+        ShowDialogue(c24);
+    }
+
+    public void c25CanvasShow()
+    {
+        ShowDialogue(c25);
+    }
+
+    public void c26CanvasShow()
+    {
+        ShowDialogue(c26);
+    }
+
+    public void c27CanvasShow()
+    {
+        ShowDialogue(c27);
+    }
+
+    public void c28CanvasShow()
+    {
+        ShowDialogue(c28);
+    }
+
+    public void c29CanvasShow()
+    {
+        ShowDialogue(c29);
+    }
+
+    public void c29CanvasClose()
+    {
+        c29.SetActive(false);
+    }
+
 
 
     private void ShowDialogue(GameObject dialogue)
@@ -178,6 +243,16 @@ public class Scene3Redlight : MonoBehaviour
         Seventeenth.SetActive(false);
         Eighteenth.SetActive(false);
         Ninteenth.SetActive(false);
+        c20.SetActive(false);
+        c21.SetActive(false);
+        c22.SetActive(false);
+        c23.SetActive(false);
+        c24.SetActive(false);
+        c25.SetActive(false);
+        c26.SetActive(false);
+        c27.SetActive(false);
+        c28.SetActive(false);
+        c29.SetActive(false);
 
         dialogue.SetActive(true);
     }
